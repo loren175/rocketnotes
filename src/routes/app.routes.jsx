301f0dbc179b50751791eab6a362ed98654a285a
Rafael Loren175 <rafael.loren175@gmail.com>
2023-11-14 10:08:49 -0300

    fallback route added

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { New } from '../pages/New'
 import { Home } from '../pages/Home'
@@ -8,10 +8,12 @@ import { Details } from '../pages/Details'
 export function AppRoutes(){
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/new" element={<New />}/>
-      <Route path="/profile" element={<Profile />}/>
-      <Route path="/details/:id" element={<Details />}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/details/:id" element={<Details />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
